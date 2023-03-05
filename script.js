8bdfb6c84e4b273927577cad0ca4657e73f5fbeb
Thiago Elias <contatothiagoelias22@gmail.com>
2023-03-04 23:46:42 -0300

    Adicionando ifsque para trocar a cor do tipo do pokemon

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function pokeapi(e) {
       <div class="nome-tipo"> 
       <h1 class="nome-poke">${data.name}</h1>
       <p class="tipo1">${data.types[0].type.name} </p>
+      
       </div>
 
       <div class="poke-imgs">
@@ -68,7 +69,42 @@ function pokeapi(e) {
       
       if (data.types[0].type.name === 'normal') {
         document.querySelector('.tipo1').style.backgroundColor = '#A4ACAF'
-      }
+      } else if (data.types[0].type.name === 'bug') {
+        document.querySelector('.tipo1').style.backgroundColor = '#89940A'
+      } else if (data.types[0].type.name === 'dark') {
+        document.querySelector('.tipo1').style.backgroundColor = '#3E2D23'
+      } else if (data.types[0].type.name === 'dragon') {
+        document.querySelector('.tipo1').style.backgroundColor = '#6756B5'
+      } else if (data.types[0].type.name === 'electric') {
+        document.querySelector('.tipo1').style.backgroundColor = '#F9B718'
+      } else if (data.types[0].type.name === 'fairy') {
+        document.querySelector('.tipo1').style.backgroundColor = '#F0ABEE'
+      } else if (data.types[0].type.name === 'fighting') {
+        document.querySelector('.tipo1').style.backgroundColor = '#141640'
+      } else if (data.types[0].type.name === 'fire') {
+        document.querySelector('.tipo1').style.backgroundColor = '#C72101'
+      } else if (data.types[0].type.name === 'flying') {
+        document.querySelector('.tipo1').style.backgroundColor = '#8D9EE8'
+      } else if (data.types[0].type.name === 'ghost') {
+        document.querySelector('.tipo1').style.backgroundColor = '#474896'
+      } else if (data.types[0].type.name === 'grass') {
+        document.querySelector('.tipo1').style.backgroundColor = '#2B570E'
+      } else if (data.types[0].type.name === 'ground') {
+        document.querySelector('.tipo1').style.backgroundColor = '#D2AF55'
+      } else if (data.types[0].type.name === 'ice') {
+        document.querySelector('.tipo1').style.backgroundColor = '#EA467F'
+      } else if (data.types[0].type.name === 'poison') {
+        document.querySelector('.tipo1').style.backgroundColor = '#752C77'
+      } else if (data.types[0].type.name === 'psychic') {
+        document.querySelector('.tipo1').style.backgroundColor = '#E64078'
+      } else if (data.types[0].type.name === 'rock') {
+        document.querySelector('.tipo1').style.backgroundColor = '#9B8638'
+      } else if (data.types[0].type.name === 'steel') {
+        document.querySelector('.tipo1').style.backgroundColor = '#8F8EA0'
+      } else if (data.types[0].type.name === 'water') {
+        document.querySelector('.tipo1').style.backgroundColor = '#1574CF'
+      }     
+    
 
       data.sprites.other['official-artwork'].front_default.classList.add("fotopoke");
     })
